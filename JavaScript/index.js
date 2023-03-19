5b8aed3831725ce6500b25378762ba4e2bd7df98
adfar-136 <adfarrasheed136@gmail.com>
2023-03-19 22:15:11 +0530

    done

---
+++ JavaScript/index.js
@@ -0,0 +1,34 @@
+// //variable declaration
+// var a= 10
+// var a = 30 // variable can be redeclared
+// console.log(a)
+// var a = 10
+// a = 30; // variable can be updated as well
+// console.log(a)
+
+// let a = 10;
+// let a = 30;// variable cant be redeclared
+// console.log(a)
+
+// let a = 10;
+// a =30;// variable it can be updated
+// console.log(a)
+// const a = 10;
+// const a = 30;// variable cant be redeclared
+// console.log(a)
+const a = 10;
+a = 30;
+console.log(a)
+
+
+
+
+// var a = 10;
+// var b = 30;
+// console.log(a+b)
+// var a = 40
+// var b = 50
+// console.log(a+b)
+// var a = 50;
+// var b = 400;
+// console.log(a+b)
---
+++ JavaScript/scope.js
@@ -0,0 +1,40 @@
+// var a = 10;
+// let b = 20;
+// const c = 30;
+
+// function adfar() {
+//   console.log(a);
+//   console.log(b);
+//   console.log(c);
+// }
+// adfar()
+
+// {
+//   var a = 10;
+//   let b = 20;
+//   const c = 30;
+//   console.log(b)
+// }
+// function adfar() {
+//   console.log(c);
+
+// }
+// adfar()
+var a = 3444440;
+let b = 44;
+const c = 4444;
+function adfar() {
+  var a = 10;
+  let b = 20;
+  const c = 30;
+  function chai() {
+    console.log(a);
+    console.log(b);
+    console.log(c);
+  }
+  chai();
+}
+
+adfar();
+
+
